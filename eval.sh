bfabb6f91e9537e58083e18020c34bc45ce8ca9e
dyllanwli <messsking@gmail.com>
2022-11-28 17:56:49 -0600

    add project

---
+++ eval.sh
@@ -0,0 +1,5 @@
+python lincls.py \
+  --workers 32 \
+  --pretrained /path-to-checkpoint/checkpoint_0099.pth.tar
+  --data_name cifar10 \
+  --save_dir ./saved_models/ \
---
+++ run.sh
@@ -0,0 +1,11 @@
+python train.py \
+  --lr=1.0 --learning-rate-scaling=sqrt \
+  --epochs=40 --batch-size=64 \
+  --loss_type dcl \
+  --gamma 0.9 \
+  --workers 32 \
+  --wd=1e-4 \
+  --data_name cifar10 \
+  --save_dir ./saved_models/ \
+  --print-freq 10 \
+  --wandb 1
