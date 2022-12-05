31730cd8714574663ee487c8914a792ef7a5eaef
dyllanwli <messsking@gmail.com>
2022-12-05 15:02:53 -0600

    add gpu

--- eval.sh
+++ eval.sh
@@ -1,6 +1,7 @@
 python lincls.py \
-  --workers 32 \
-  --epochs 20 \
+  --workers 16 \
+  --epochs 90 \
   --pretrained ./saved_models_dynamic_gamma/20221013_cifar10_resnet50_sogclr-128-2048_bz_64_E400_WR10_lr_0.250_linear_wd_0.0001_t_0.3_g_0.6_lars/checkpoint_0399.pth.tar \
   --data_name cifar10 \
+  --gpu 0 \
   --save_dir ./saved_models/
